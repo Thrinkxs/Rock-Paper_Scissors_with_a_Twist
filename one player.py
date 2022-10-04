eecb162324d106ad503063d3cf469390f89d1682
Thrinkxs <emmanuelozigue@gmail.com>
2022-10-04 23:47:50 +0200

    solo built from scratch

--- one player.py
+++ one player.py
@@ -6,8 +6,16 @@ usr_math = {"Rock": 3, "Paper": 2, "Scissors": 1}
 while game != 'no':
     game_start = ['Rock', 'Paper', 'Scissors']
     user_game = random.choice(game_start)
-    user_choice = input("Rock(r), Paper(p), or Scissors(s)\n")
+    user_choice = input("Rock, Paper, or Scissors\n")
     user_choice = user_choice.lower()
+    user_game = user_game.lower()
     print(user_game)
     print(user_choice)
+    again = input("Do you want to play again? yes/no?\n")
+    again = again.lower()
+    if again == "no":
+        print("Game Over")
+        break
+    else:
+        continue
     #break
